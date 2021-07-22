c11ecf2f09a55d89764cafe2b58dbdb47e91751e
Siddhesh2811 <wani.siddhesh.siddhesh2@gmail.com>
2021-07-22 11:49:38 +0530

    companydashboard updates html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('',views.studashboard),
     path('comdashboard',views.comdashboard),
     path('about',aboutus),
+    path('comprofile',views.comprofile),
+    path('complacement',views.complacement),
+    path('cominternship',views.cominternship),
+    path('comall',views.comall),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -12,3 +12,23 @@ def comdashboard(request):
     suser = studentUser.objects.all()
     cuser = companyUser.objects.all()
     return render(request, 'comdashboard.html',{'suser': suser,'cuser':cuser})
+
+def comprofile(request):
+    suser = studentUser.objects.all()
+    cuser = companyUser.objects.all()
+    return render(request, 'comprofile.html',{'suser': suser,'cuser':cuser})
+
+def complacement(request):
+    suser = studentUser.objects.all()
+    cuser = companyUser.objects.all()
+    return render(request, 'complacement.html',{'suser': suser,'cuser':cuser})
+
+def cominternship(request):
+    suser = studentUser.objects.all()
+    cuser = companyUser.objects.all()
+    return render(request, 'cominternship.html',{'suser': suser,'cuser':cuser})
+
+def comall(request):
+    suser = studentUser.objects.all()
+    cuser = companyUser.objects.all()
+    return render(request, 'comall.html',{'suser': suser,'cuser':cuser})
