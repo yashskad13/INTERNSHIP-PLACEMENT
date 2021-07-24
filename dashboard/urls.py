ea0cdfbd03857f54589ec5bb2fb8f8bc2ff9ca21
Siddhesh2811 <wani.siddhesh.siddhesh2@gmail.com>
2021-07-24 15:04:14 +0530

    dashmodify

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('complacement',views.complacement),
     path('cominternship',views.cominternship),
     path('comall',views.comall),
+    path('stuprofile',views.stuprofile),
+    path('stuplacement',views.stuplacement),
+    path('stuinternship',views.stuinternship),
+    path('stuall',views.stuall),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,34 +1,37 @@
 from django.shortcuts import render
 from accounts.models import studentUser,companyUser
 # Create your views here.
-
+suser = studentUser.objects.all()
+cuser = companyUser.objects.all()
 
 def studashboard(request):
-    suser = studentUser.objects.all()
-    cuser = companyUser.objects.all()
     return render(request, 'studashboard.html',{'suser': suser,'cuser':cuser})
 
+def stuprofile(request):
+    return render(request, 'stuprofile.html',{'suser': suser,'cuser':cuser})
+
+def stuplacement(request):
+    return render(request, 'stuplacement.html',{'suser': suser,'cuser':cuser})
+
+def stuinternship(request):
+    return render(request, 'stuinternship.html',{'suser': suser,'cuser':cuser})
+
+def stuall(request):
+    return render(request, 'stuall.html',{'suser': suser,'cuser':cuser})
+
+
+
 def comdashboard(request):
-    suser = studentUser.objects.all()
-    cuser = companyUser.objects.all()
     return render(request, 'comdashboard.html',{'suser': suser,'cuser':cuser})
 
 def comprofile(request):
-    suser = studentUser.objects.all()
-    cuser = companyUser.objects.all()
     return render(request, 'comprofile.html',{'suser': suser,'cuser':cuser})
 
 def complacement(request):
-    suser = studentUser.objects.all()
-    cuser = companyUser.objects.all()
     return render(request, 'complacement.html',{'suser': suser,'cuser':cuser})
 
 def cominternship(request):
-    suser = studentUser.objects.all()
-    cuser = companyUser.objects.all()
     return render(request, 'cominternship.html',{'suser': suser,'cuser':cuser})
 
 def comall(request):
-    suser = studentUser.objects.all()
-    cuser = companyUser.objects.all()
     return render(request, 'comall.html',{'suser': suser,'cuser':cuser})
