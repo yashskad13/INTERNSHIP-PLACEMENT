c16b6abebb3acaa3c97bdfb89896fbe42b1073fe
RoshanMhatre <roshanmhatre989@gmail.com>
2021-07-26 12:42:03 +0530

    updates: company proflle and student profile

--- dashboard/templatetags/student.py
+++ dashboard/templatetags/student.py
@@ -2,59 +2,66 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter(name="is_student")
 def is_student(user, suser):
     for u in suser:
-        if(u.username==user.username):
-               return True
+        if(u.username == user.username):
+            return True
     return False
+
 
 @register.filter(name="student_yourname")
 def student_yourname(username, suser):
     for u in suser:
-        if(u.username==username):
-               return u.yourname
+        if(u.username == username):
+            return u.yourname
+
 
 @register.filter(name="email")
 def email(username, suser):
     for u in suser:
-        if(u.username==username):
-               return u.email
+        if(u.username == username):
+            return u.email
+
 
 @register.filter(name="student_branch")
 def student_branch(username, suser):
     for u in suser:
-        if(u.username==username):
-               return u.branch
+        if(u.username == username):
+            return u.branch
+
 
 @register.filter(name="yog")
 def yog(username, suser):
     for u in suser:
-        if(u.username==username):
-               return u.yog
+        if(u.username == username):
+            return u.yog
+
 
 @register.filter(name="contact")
 def contact(username, suser):
     for u in suser:
-        if(u.username==username):
-               return u.contact
+        if(u.username == username):
+            return u.contact
+
 
 @register.filter(name="company_name")
 def company_name(username, cuser):
     for u in cuser:
-        if(u.username==username):
-               return u.companyname
+        if(u.username == username):
+            return u.companyname
+
 
 @register.filter(name="address")
 def address(username, cuser):
     for u in cuser:
-        if(u.username==username):
-               return u.address
+        if(u.username == username):
+            return u.address
+
 
 @register.filter(name="company_email")
 def company_email(username, cuser):
     for u in cuser:
-        if(u.username==username):
-               return u.companyemail
-
-
+        if(u.username == username):
+            return u.companyemail
